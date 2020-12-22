7e8bfe91c9549823566b5bbbc172da7425e42f9a
Justin Hurst <hurstjustin502@gmail.com>
2020-12-21 20:44:24 -0500

    added geolocation for incoming weather API

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,11 @@
+import useGeoLocation from './GeoLocation';
 
 function Home() {
+    const location = useGeoLocation();
     return (
-        <div>This is the Home component</div>
+        <div>This is the Home component
+            <h1>{ location.loaded ? JSON.stringify(location.coordinates) : "No location data"}</h1>
+        </div>
     )
 }
 
